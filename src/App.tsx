c2dea2c88cbf0d090b1e5622ac2f2a79f800603b
nghiahu <nghia120425@gmail.com>
2024-06-27 07:55:56 +0700

    up

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Count from "./components/bt01";
+import Bt02 from "./components/Bt02";
+import Bt03 from "./components/Bt03";
+import Bt04 from "./components/Bt04";
+import Bt05 from "./components/Bt05";
+import Bt06 from "./components/Bt06";
+import Bt07 from "./components/Bt07";
+import Bt08 from "./components/Bt08";
+
+export default function App() {
+  return (
+    <div>
+      <Count></Count>
+      <Bt02></Bt02>
+      <Bt03></Bt03>
+      <Bt04></Bt04>
+      <Bt05></Bt05>
+      <Bt06></Bt06>
+      <Bt07></Bt07>
+      <Bt08></Bt08>
+    </div>
+  );
+}
